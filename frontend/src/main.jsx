74083aba4a0850234dffbe1e70647b47d49a7631
robi450 <rtcela@gmail.com>
2025-08-10 23:35:42 -0400

    fix(router): use Vite BASE_URL as BrowserRouter basename

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import './theme.css';
-
+import "./theme.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
